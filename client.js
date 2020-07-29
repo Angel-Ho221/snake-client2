242bf4b8f5c9163466512ee06413d32a401ad827
Angel <angel.ho@ryerson.ca>
2020-07-29 20:42:01 +0000

    added all const in here

--- client.js
+++ client.js
@@ -1,19 +1,47 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50541,
-  
-    
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on("data", function(message){
+  conn.setEncoding('utf8');
+
+  conn.on("connect", function () {
+    conn.write("Name: AH");
+  });
+
+  conn.on("connect", function (connect) {
+    console.log("Successfully connected to game server!");
+  });
+
+  conn.on("data", function (message) {
     //do smthing when i receive the data
     console.log(message);
+  })
 
-})
+  // conn.on('connect', () => {
+  //   conn.write("Move: up")
+  //   setTimeout(() => {
+  //     process.stdout.write(conn.write("Move: left"))
+  //   }, 100);
+  //   setTimeout(() => {
+  //     process.stdout.write(conn.write("Move: up"))
+  //   }, 200);
+  //   setTimeout(() => {
+  //     process.stdout.write(conn.write("Move: right"))
+  //   }, 300);
+  //   setInterval(() => {
+  //     process.stdout.write(conn.write("Move: left"))
+  //   }, 200);
+  //   setTimeout(() => {
+  //     process.stdout.write(conn.write("Move: down"))
+  //   }, 400);
+  // })
+
   return conn;
 }
 
