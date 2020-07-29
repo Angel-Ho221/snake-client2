f9cccc21e7106c414e6e90aa99b6c241010dc701
Angel <angel.ho@ryerson.ca>
2020-07-29 18:35:08 +0000

    added handleuser and setup input

--- play.js
+++ play.js
@@ -2,3 +2,19 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function (data) {
+    if (key === '\u0003') {
+      console.log("exiting program")
+      process.exit();
+
+    }
+  }
+  return stdin;
+}
